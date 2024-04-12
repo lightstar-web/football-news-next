df79c4f156ea4cdc8f11c555453a04ffc95ff2ae
lightstar0412 <dev.star.man0412@gmail.com>
2024-04-12 16:08:17 +0800

    update

---
+++ pages/_app.js
@@ -0,0 +1,41 @@
+import '../public/assets/css/font-awesome.css';
+import '../public/assets/css/main.css';
+import '../public/assets/css/loading.css';
+import '../public/assets/css/error.css';
+
+const { default: AbortController } = require("abort-controller");
+const { default: fetch, Headers, Request, Response } = require("node-fetch");
+
+Object.assign(globalThis, {
+    fetch,
+    Headers,
+    Request,
+    Response,
+    AbortController,
+});
+
+import App from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import client from "../lib/apollo-client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+function MyApp({ Component, pageProps }) {
+    return <Auth0Provider
+        domain={process.env.AUTH_DOMAIN}
+        clientId={process.env.AUTH_CLIENTID}
+        redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    </Auth0Provider>
+}
+
+MyApp.getInitialProps = async (appContext) => {
+    const appProps = await App.getInitialProps(appContext);
+    return { ...appProps }
+}
+
+export default MyApp
+
+
